305652c36f24b82b613eed652db94fe343e52190
Jon Bailey <jbailey2900@users.noreply.github.com>
2019-03-29 10:49:46 -0400

    Modified to handle multiple rows and a column

--- CS/MultiSelectionEditingHelper.cs
+++ CS/MultiSelectionEditingHelper.cs
@@ -1,31 +1,71 @@
 using System;
-using System.Collections.Generic;
 using System.Windows.Forms;
-using DevExpress.XtraGrid.Views.Grid;
 using DevExpress.Utils;
+using DevExpress.XtraGrid.Columns;
 using DevExpress.XtraGrid.Views.Base;
-using DevExpress.XtraGrid.Views.Grid.ViewInfo;
+using DevExpress.XtraGrid.Views.Grid;
 
-namespace WindowsApplication1
+namespace WinFormTest
 {
-    public class MultiSelectionEditingHelper
+    public class MultiSelectEditingHelper
     {
-        public MultiSelectionEditingHelper(GridView view)
+        private GridView view;
+        private GridColumn[] columns;
+        private bool lockEvents;
+
+        public MultiSelectEditingHelper(GridView view, GridColumn[] changeableColumns)
         {
             this.view = view;
-            this.view.OptionsBehavior.EditorShowMode = EditorShowMode.MouseDownFocused;
+            this.columns = changeableColumns;  
+            this.view.OptionsBehavior.EditorShowMode = DevExpress.Utils.EditorShowMode.MouseDown;
             this.view.MouseUp += view_MouseUp;
             this.view.CellValueChanged += view_CellValueChanged;
             this.view.MouseDown += view_MouseDown;
         }
 
-        private GridView view;
-
-        void view_MouseDown(object sender, MouseEventArgs e)
+        private void view_CellValueChanged(object sender, CellValueChangedEventArgs e)
         {
-            if (GetInSelectedCell(e))
+            OnCellValueChanged(e);
+        }
+
+        private void OnCellValueChanged(CellValueChangedEventArgs e)
+        {
+            if (lockEvents) return;
+            lockEvents = true;
+            SetSelectedCellsValue(e.Value);
+            lockEvents = false;
+        }
+
+        private void SetSelectedCellsValue(object value)
+        {
+            try
             {
-                GridHitInfo hi = view.CalcHitInfo(e.Location);
+                view.BeginUpdate();
+                var rows = view.GetSelectedRows();
+                foreach (var row in rows) view.SetRowCellValue(row, view.FocusedColumn, value);
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                view.EndUpdate();
+            }
+        }
+
+        private bool GetInSelectedRow(MouseEventArgs e)
+        {
+            var hi = view.CalcHitInfo(e.Location);
+            if (!hi.InRowCell) return false;
+            foreach (var column in columns) if (hi.Column == column) return true;
+            return false;
+        }
+
+        private void view_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (GetInSelectedRow(e))
+            {
+                var hi = view.CalcHitInfo(e.Location);
                 if (view.FocusedRowHandle == hi.RowHandle)
                 {
                     view.FocusedColumn = hi.Column;
@@ -34,43 +74,9 @@ namespace WindowsApplication1
             }
         }
 
-        void view_CellValueChanged(object sender, CellValueChangedEventArgs e)
+        private void view_MouseUp(object sender, MouseEventArgs e)
         {
-            OnCellValueChanged(e);
-        }
-
-        bool lockEvents;
-        private void OnCellValueChanged(CellValueChangedEventArgs e)
-        {
-            if (lockEvents)
-                return;
-            lockEvents = true;
-            SetSelectedCellsValues(e.Value);
-            lockEvents = false;
-        }
-
-        private void SetSelectedCellsValues(object value)
-        {
-            try
-            {
-                view.BeginUpdate();
-                GridCell[] cells = view.GetSelectedCells();
-                foreach (GridCell cell in cells)
-                    view.SetRowCellValue(cell.RowHandle, cell.Column, value);
-            }
-            catch (Exception ex) { }
-            finally { view.EndUpdate(); }
-        }
-
-        private bool GetInSelectedCell(MouseEventArgs e)
-        {
-            GridHitInfo hi = view.CalcHitInfo(e.Location);
-            return hi.InRowCell && view.IsCellSelected(hi.RowHandle, hi.Column);
-        }
-
-        void view_MouseUp(object sender, MouseEventArgs e)
-        {
-            bool inSelectedCell = GetInSelectedCell(e);
+            var inSelectedCell = GetInSelectedRow(e);
             if (inSelectedCell)
             {
                 DXMouseEventArgs.GetMouseArgs(e).Handled = true;
